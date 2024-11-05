01b056db8d6691b6b102d2418cd6684f9b184c0d
zhaoyifan <1784848264@qq.com>
2024-11-05 09:11:35 +0800

    优化文档文件打包指令错误问题

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+# /bin/bash
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 重新打包组件库
+#pnpm build
+
+# 打包生成静态文件
+#npm docs:build
+
+# 进入待发布的 dist/ 目录
+cd ../docs/.vuepress/dist/
+
+# 提交打包静态网站到 pages 分支
+git init
+git add .
+git commit -m 'deploy'
+
+# 部署到 https://<username>.github.io/<repo>
+git push -f git@github.com:zhaoyifan-admin/Tantanzi-ui.git master:pages
+
+## 提交所有代码到github
+#cd ../../../
+#git add .
+#git cm -m 'update'
+#git push
