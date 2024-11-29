01f6bd1313a36b186773aa73395ded05bda3f748
zhaoyifan <1784848264@qq.com>
2024-11-29 09:48:10 +0800

    修正

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
 
-import install from "tantanzi-ui";
-import "tantanzi-ui/lib/tantanzi-ui.css"
-Vue.use(install);
+// import install from "tantanzi-ui";
+// import "tantanzi-ui/lib/tantanzi-ui.css"
+// Vue.use(install);
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
